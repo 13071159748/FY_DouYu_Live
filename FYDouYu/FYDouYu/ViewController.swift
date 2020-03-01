a869f9f771607fb03e4d1fe915ca368f22eb6361
fengyong <18231855680@163.com>
2020-03-01 12:29:36 +0800

    1.project

---
+++ FYDouYu/FYDouYu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FYDouYu
+//
+//  Created by 冯勇 on 2020/3/1.
+//  Copyright © 2020 冯勇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FYDouYu/FYDouYuTests/FYDouYuTests.swift
@@ -0,0 +1,34 @@
+//
+//  FYDouYuTests.swift
+//  FYDouYuTests
+//
+//  Created by 冯勇 on 2020/3/1.
+//  Copyright © 2020 冯勇. All rights reserved.
+//
+
+import XCTest
+@testable import FYDouYu
+
+class FYDouYuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
